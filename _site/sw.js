71d0f3f6e0f33d9131f752b2ac4d950d9d02dca5
pablojosecodes <pablojrhansen@gmail.com>
2023-12-20 21:19:37 +0100

    testing _sass

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703088942';
+const cacheName = 'chirpy-1703103567';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
